8d8000b8d4e3e39f3ee43007dc2a40e7b79d7898
Prajwal <prajwal.akhuj@squareops.com>
2023-05-31 13:27:56 +0530

    modified resource group name and updated comments for storage account resources

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 data "azuread_client_config" "current" {}
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
+  name     = format("%s-%s-name", var.azurerm_resource_group, var.environment)
   location = var.resource_group_location
   tags = local.tags
 }
@@ -28,8 +28,8 @@ resource "azurerm_storage_account" "storage_account" {
   name                     = format("%s%s", var.storage_account_name, var.environment)
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = "Standard" # Standard storage account type for blobs, file shares, queues, and tables. Recommended for most scenarios using Azure Storage. If you want support for network file system (NFS) in Azure Files, use the premium file shares account type.
+  account_replication_type = "LRS" # Locally redundant storage (LRS) copies your data synchronously three times within a single physical location in the primary region. LRS is the least expensive replication option, but isn't recommended for applications requiring high availability or durability.
   tags = local.tags
 }
 
