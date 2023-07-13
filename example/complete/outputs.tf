4ec94cb6e862043a096ab7715170c57133c84692
Prajwal <prajwal.akhuj@squareops.com>
2023-07-13 12:41:37 +0530

    formatted docs & updated provider version in README.md

--- example/complete/outputs.tf
+++ example/complete/outputs.tf
@@ -1,14 +1,14 @@
 output "terraform_state_resource_group_name" {
   description = "Specify the resource group name in which an Storage account will be created by the module."
-  value = module.backend.terraform_state_resource_group_name
+  value       = module.backend.terraform_state_resource_group_name
 }
 
 output "terraform_state_storage_account" {
   description = "Specify the storage account name in which an Storage container will be created by the module."
-  value = module.backend.terraform_state_storage_account
+  value       = module.backend.terraform_state_storage_account
 }
 
 output "terraform_state_storage_container_name" {
   description = "Specify the storage container name in where tfstate will be stored by the module."
-  value =module.backend.terraform_state_storage_container_name
+  value       = module.backend.terraform_state_storage_container_name
 }
